ddeb04e4c8a8f158960e2b4ab5cf4c1ed5e29b46
Charles <carlosjosedesign@gmail.com>
2022-10-06 19:37:17 -0300

    finish

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ Design a front-end for an email client that makes API calls to send and receive 
 You will need [Python](https://www.python.org/) with [Django library](https://www.djangoproject.com/)  
 ```
 • Clone the package  
-• python manage.py makemigrations auctions  
+• python manage.py makemigrations
+• python manage.py makemigrations mail  
 • python manage.py migrate  
 • python manage.py runserver  
 • python manage.py createsuperuser
@@ -18,28 +19,28 @@ You will need [Python](https://www.python.org/) with [Django library](https://ww
 
 ## Specification
 Using JavaScript, HTML, and CSS, complete the implementation of your single-page-app email client inside of inbo .js (and not additional or other files; for grading purposes, we’re only going to be considering inbo .js!). You must fulfill the following requirements:
-- [ ] Send Mail: When a user submits the email composition form, add JavaScript code to actually send the email.
-  - [ ] You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.
-  - [ ]Once the email has been sent, load the user’s sent mailbo .
-- [ ]Mailbo : When a user visits their Inbo , Sent mailbo , or Archive, load the appropriate mailbo .
-  - [ ]You’ll likely want to make a GET request to /emails/<mailbo > to request the emails for a particular mailbo .
-  - [ ]When a mailbo  is visited, the application should first query the API for the latest emails in that mailbo .
-  - [ ]When a mailbo  is visited, the name of the mailbo  should appear at the top of the page (this part is done for you).
-  - [ ]Each email should then be rendered in its own bo  (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
-  - [ ]If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
-- [ ]View Email: When a user clicks on an email, the user should be taken to a view where they see the content of that email.
-  - [ ]You’ll likely want to make a GET request to /emails/<email_id> to request the email.
-  - [ ]Your application should show the email’s sender, recipients, subject, timestamp, and body.
-  - [ ]You’ll likely want to add an additional div to inbo .html (in addition to emails-view and compose-view) for displaying the email. Be sure to update your code to hide and show the right views when navigation options are clicked.
-  - [ ]See the hint in the Hints section about how to add an event listener to an HTML element that you’ve added to the DOM.
-  - [ ]Once the email has been clicked on, you should mark the email as read. Recall that you can send a PUT request to /emails/<email_id> to update whether an email is read or not.
-- [ ]Archive and Unarchive: Allow users to archive and unarchive emails that they have received.
-  - [ ]When viewing an Inbo  email, the user should be presented with a button that lets them archive the email. When viewing an Archive email, the user should be presented with a button that lets them unarchive the email. This requirement does not apply to emails in the Sent mailbo .
-  - [ ]Recall that you can send a PUT request to /emails/<email_id> to mark an email as archived or unarchived.
-  - [ ]Once an email has been archived or unarchived, load the user’s inbo .
-- [ ]Reply: Allow users to reply to an email.
-  - [ ]When viewing an email, the user should be presented with a “Reply” button that lets them reply to the email.
-  - [ ]When the user clicks the “Reply” button, they should be taken to the email composition form.
-  - [ ]Pre-fill the composition form with the recipient field set to whoever sent the original email.
-  - [ ]Pre-fill the subject line. If the original email had a subject line of foo, the new subject line should be Re: foo. (If the subject line already begins with Re: , no need to add it again.)
-  - [ ]Pre-fill the body of the email with a line like "On Jan 1 2020, 12:00 AM foo@e ample.com wrote:" followed by the original te t of the email.
+- [x] Send Mail: When a user submits the email composition form, add JavaScript code to actually send the email.
+  - [x] You’ll likely want to make a POST request to /emails, passing in values for recipients, subject, and body.
+  - [x]Once the email has been sent, load the user’s sent mailbox .
+- [x]mailbox : When a user visits their Inbo , Sent mailbox , or Archive, load the appropriate mailbox .
+  - [x]You’ll likely want to make a GET request to /emails/<mailbox > to request the emails for a particular mailbox .
+  - [x]When a mailbox  is visited, the application should first query the API for the latest emails in that mailbox .
+  - [x]When a mailbox  is visited, the name of the mailbox  should appear at the top of the page (this part is done for you).
+  - [x]Each email should then be rendered in its own bo  (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
+  - [x]If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+- [x]View Email: When a user clicks on an email, the user should be taken to a view where they see the content of that email.
+  - [x]You’ll likely want to make a GET request to /emails/<email_id> to request the email.
+  - [x]Your application should show the email’s sender, recipients, subject, timestamp, and body.
+  - [x]You’ll likely want to add an additional div to inbox .html (in addition to emails-view and compose-view) for displaying the email. Be sure to update your code to hide and show the right views when navigation options are clicked.
+  - [x]See the hint in the Hints section about how to add an event listener to an HTML element that you’ve added to the DOM.
+  - [x]Once the email has been clicked on, you should mark the email as read. Recall that you can send a PUT request to /emails/<email_id> to update whether an email is read or not.
+- [x]Archive and Unarchive: Allow users to archive and unarchive emails that they have received.
+  - [x]When viewing an Inbox  email, the user should be presented with a button that lets them archive the email. When viewing an Archive email, the user should be presented with a button that lets them unarchive the email. This requirement does not apply to emails in the Sent mailbox .
+  - [x]Recall that you can send a PUT request to /emails/<email_id> to mark an email as archived or unarchived.
+  - [x]Once an email has been archived or unarchived, load the user’s inbox .
+- [x]Reply: Allow users to reply to an email.
+  - [x]When viewing an email, the user should be presented with a “Reply” button that lets them reply to the email.
+  - [x]When the user clicks the “Reply” button, they should be taken to the email composition form.
+  - [x]Pre-fill the composition form with the recipient field set to whoever sent the original email.
+  - [x]Pre-fill the subject line. If the original email had a subject line of foo, the new subject line should be Re: foo. (If the subject line already begins with Re: , no need to add it again.)
+  - [x]Pre-fill the body of the email with a line like "On Jan 1 2020, 12:00 AM foo@e ample.com wrote:" followed by the original te t of the email.
--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -3,15 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
-  document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+  document.querySelector('#archive').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+
+  const form = document.querySelector('#compose-form')
+  form.addEventListener('submit', submitEmail);
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
+function changeButton(mailbox){
+  document.querySelectorAll('.boxbtn').forEach( btn => {
+    btn.classList.add('btn-outline-primary')
+    btn.classList.remove('btn-primary');
+  })
+  document.getElementById(mailbox).classList.remove('btn-outline-primary')
+  document.getElementById(mailbox).classList.add('btn-primary');
+}
 
+function compose_email() {
+  changeButton('compose')
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -22,12 +34,338 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
 function load_mailbox(mailbox) {
-  
+  changeButton(mailbox)
+  let mailView = document.querySelector('#emails-view');
+
   // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
+  mailView.style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
-  document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  mailView.innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  //get Emails
+  getEmails(mailbox, mailView);
+
+ 
+}
+
+function submitEmail(event){
+  event.preventDefault();
+  let messages = document.querySelector('.alert');
+  if(messages){
+    messages.remove();
+  }
+
+  const recipients = document.querySelector('#compose-recipients').value
+  const subject = document.querySelector('#compose-subject').value
+  const mailBody = document.querySelector('#compose-body').value
+  const form = document.querySelector('#compose-form')
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: recipients,
+        subject: subject,
+        body: mailBody
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      
+      if(result.error){
+        createMessage('error', result.error, form)
+      }else{
+        load_mailbox('sent')
+        createMessage('success', result.message, document.getElementById('emails-view'))
+      }
+      
+  });
+
+  return false;
+}
+
+function createMessage(type, message, element){
+  messageBox = document.createElement('div');
+  messageBox.classList = `alert alert-${ type == 'error' ? 'danger' : 'success'}`
+  messageBox.innerHTML = message
+
+  closeButton = document.createElement('button')
+  closeButton.classList = 'close'
+  closeButton.innerHTML = ' <span aria-hidden="true">&times;</span>'
+  closeButton.addEventListener('click', closemessage)
+  messageBox.append(closeButton)
+
+  element.prepend(messageBox);
+}
+
+function closemessage(){
+  document.querySelector('.alert').remove()
+}
+
+function  getEmails(mailbox, view){
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails)
+    if(emails.length > 0){
+      if(mailbox != 'sent'){
+        createArchiveButton(mailbox, view)
+        createUnreadButton(mailbox, view)
+      }
+    }else{
+      let noEmails = document.createElement('p')
+      noEmails.innerHTML = 'No emails here'
+      view.append(noEmails)
+      return
+    }
+
+    emails.forEach(email => {
+      let mailLine = document.createElement('div');
+      mailLine.classList = `mailLine d-flex justify-content-between align-items-center ${email.read ? "read" : ""}`
+      mailLine.addEventListener('click', () => { openEmail(email.id)})
+
+      let mailCheck = document.createElement('label')
+      mailCheck.classList = 'mailCheck';
+      mailCheck.for = `check${email.id}`
+     
+      let  mailCheckInput = document.createElement('input')
+       mailCheckInput.id = `check${email.id}`
+       mailCheckInput.classList = 'mailCheckInput';
+       mailCheckInput.type = 'checkbox'
+       mailCheckInput.value = email.id
+      mailCheck.append( mailCheckInput)
+
+      let mailFrom = document.createElement('div')
+      mailFrom.classList = 'mailFrom'
+      mailFrom.innerHTML = email.sender
+
+      let mailContent = document.createElement('div');
+      mailContent.classList = 'mailContent'
+      let text = `${email.subject} - ${email.body}`
+      let contentText = text.slice(0, 80);
+      if(text.length > 80){
+        contentText = contentText + '...'
+      }
+      mailContent.innerHTML = contentText
+
+      let mailDate = document.createElement('div')
+      mailDate.classList = 'mailDate'
+      mailDate.innerHTML = email.timestamp
+
+      if(mailbox != 'sent'){
+        mailLine.append(mailCheck)
+      }else{
+        mailFrom.innerHTML = 'To: ' + email.recipients
+        mailFrom.classList.add('sents')
+      }
+
+      mailLine.append(mailFrom, mailContent, mailDate)
+
+      view.append(mailLine)
+    });
+    
+  });
+}
+
+function createArchiveButton(mailbox, view){
+  let archiveButton = document.createElement('button')
+  if(mailbox == 'inbox'){
+    archiveButton.innerHTML = 'Archive'
+    archiveButton.classList = 'btn btn-info my-2'
+    archiveButton.addEventListener('click', archiveMails)
+  }else if(mailbox == 'archive'){
+    archiveButton.innerHTML = 'Unarchive'
+    archiveButton.classList = 'btn btn-warning my-2'
+    archiveButton.addEventListener('click', unArchiveMails)
+  }
+  view.append(archiveButton);
+}
+
+function createUnreadButton(mailbox, view){
+  let unreadButton = document.createElement('button')
+  unreadButton.innerHTML = 'Unread'
+  unreadButton.classList = 'btn btn-primary my-2 ml-3'
+  unreadButton.addEventListener('click', () => { unreadMails(mailbox) })
+  view.append(unreadButton);
+}
+
+async function getSingleEmail(id){
+  const response =  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+      return  email
+  });
+  return response
+}
+
+async function openEmail(id){
+  console.log(event.target.classList[0])
+  if(event.target.classList[0] == 'mailCheck' || event.target.classList[0] == 'mailCheckInput'){
+    return
+  }
+
+  markAsRead(id)
+
+  const email = await  getSingleEmail(id);
+  const mailView = document.querySelector('#emails-view');
+
+  let mailPage = document.createElement('div')
+  mailPage.id = 'mailPage'
+
+  let mailHead = document.createElement('div')
+  mailHead.classList = 'mailHead'
+  mailHead.innerHTML = `
+  <h4>Subject: ${email.subject.charAt(0).toUpperCase() + email.subject.slice(1)}</h4>
+  <p>From: ${email.sender}</p>
+  <p>To: ${email.recipients[0]}</p>
+  <p>Date: ${email.timestamp}</p>
+  `
+
+  let mailBody = document.createElement('div')
+  mailBody.classList = 'mailBody'
+  mailBody.innerHTML = email.body
+
+  let mailFooter = document.createElement('div')
+  mailFooter.classList = 'mailFooter row mx-0'
+  
+  let mailFooterArchive = document.createElement('div')
+  mailFooterArchive.classList = "col-3 ml-auto my-4 pr-0"
+
+  if(!email.archived){
+    mailFooterArchive.innerHTML = '<button class="btn btn-primary w-100">Archive</button>'
+    mailFooterArchive.addEventListener('click', () => { archiveOne(email.id, true)})
+  }else{
+    mailFooterArchive.innerHTML = '<button class="btn btn-warning w-100">Unarchive</button>'
+    mailFooterArchive.addEventListener('click', () => { archiveOne(email.id, false)})
+  }
+
+  let mailFooterReply = document.createElement('div')
+  mailFooterReply.classList = "col-6 ml-3 my-4 pr-0"
+  mailFooterReply.innerHTML = '<button class="btn btn-success w-100">Reply</button>'
+  mailFooterReply.addEventListener('click', () => { reply(email)})
+
+  if(email.sender != document.querySelector('h2').innerHTML || email.archived){
+    mailFooter.append(mailFooterArchive)
+  }else{
+    mailFooterReply.classList.add('ml-auto')
+  }
+  mailFooter.append(mailFooterReply)
+
+  mailPage.append(mailHead, mailBody)
+
+  mailView.innerHTML = ''
+  mailView.append(mailPage, mailFooter)
+}
+
+function reply(email){
+    // Show compose view and hide other views
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'block';
+
+    const subject = (email.subject.includes("Re:") ? '' : 'Re: ') + email.subject
+    const body = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`
+  
+    // Clear out composition fields
+    document.querySelector('#compose-recipients').value = email.sender;
+    document.querySelector('#compose-subject').value = subject;
+    document.querySelector('#compose-body').value = body;
+}
+
+function archiveMails(){
+  let checks = document.querySelectorAll('.mailCheckInput:checked')
+  if(checks.length > 0){
+    let c = 0;
+    checks.forEach( check => {
+      if(check.checked ){
+        markAsArchived(check.value, true, (c == checks.length - 1))
+        c++
+      }
+     })
+
+  }
+
+}
+function unArchiveMails(){
+  let checks = document.querySelectorAll('.mailCheckInput:checked')
+  if(checks.length > 0){
+    let c = 0;
+    checks.forEach( check => {
+      if(check.checked ){
+        markAsArchived(check.value, false, (c == checks.length - 1))
+        c++
+      }
+     })
+
+  }
+}
+
+async function archiveOne(id, mode){
+  await markAsArchived(id, mode, true)
+}
+
+async function markAsArchived(id, archived = true, last=false){
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        archived: archived
+    })
+  }).then( () => { 
+    if(last){
+      if(archived){
+        load_mailbox('archive')
+        createMessage('success', 'Mails archived success', document.getElementById('emails-view'))
+      }else{
+        load_mailbox('inbox')
+        createMessage('success', 'Mails unarchived success', document.getElementById('emails-view'))
+      }
+    }
+   
+    return true 
+  })
+}
+
+async function unreadMails(mailbox){
+  let checks = document.querySelectorAll('.mailCheckInput:checked')
+  if(checks.length > 0){
+    let c = 0;
+    checks.forEach( check => {
+      if(check.checked ){
+        console.log(c)
+        markAsUnRead(check.value, mailbox, (c == checks.length - 1))
+        c++
+      }
+     })
+   
+  }
+}
+
+async function markAsRead(id){
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  }).then( () => { return true })
+}
+
+function markAsUnRead(id, mailbox, last=false){
+  fetch(`/emails/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: false
+    })
+  }).then( () => { 
+    if(last){
+      if(mailbox == 'archive'){
+        load_mailbox('archive')
+      }else{
+        load_mailbox('inbox')
+      }
+      createMessage('success', 'Mails marked as unread', document.getElementById('emails-view'))
+    }
+    return true
+  })
 }
